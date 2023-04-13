576b12df9143d31c6590cbfafad7d814fad73ba8
GMartins <guilherme.martinst.dev@gmail.com>
2023-04-13 10:35:47 -0300

    feat navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <router-link to="/users">Lista de Usuários</router-link>
+            <router-link to="/register">Registrar Usuário</router-link>
+            <router-link to="/update">Editar Usuário</router-link>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Navbar'
+    
+}
+
+</script>
