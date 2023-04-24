058ec5922c08f1ccda8783e19d1bdb901a001e53
GMartins <guilherme.martinst.dev@gmail.com>
2023-04-24 12:59:59 -0300

    feat(state): localstorage e state ok

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <div>
+            <div>
+                
+            </div>
             <router-link to="/users">Lista de Usuários</router-link>
             <router-link to="/register">Registrar Usuário</router-link>
             <router-link to="/update">Editar Usuário</router-link>
-
-            <a href="profile"> {  }</a>
+            <div>
+                <a href="profile">Meu Perfil</a>
+            </div>
+            
         </div>
     </div>
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -32,9 +32,9 @@
 <script lang="ts">
 import router from "@/router";
 import { defineComponent } from "vue";
-import { request } from "@/api";
 import authService from "@/services/authService";
 import store from "@/store";
+import { SocketModule } from "@/services/socket";
 
 export default defineComponent({
   name: "LoginView",
@@ -53,11 +53,15 @@ export default defineComponent({
           localStorage.setItem("token", response.data.access_token);
           localStorage.setItem("sessionId", response.data._id);
           localStorage.setItem("role", response.data.role);
-          console.log("logResData", response.data)
-          store.commit("setId", response.data._id)
-          store.commit("setName", response.data.name)
-          store.commit("setToken", response.data.access_token)
-          store.commit("setRole", response.data.role)
+          localStorage.setItem("id", response.data._id);
+          localStorage.setItem("email", response.data.email);
+          localStorage.setItem("name", response.data.name);
+          console.log("logResData", response.data);
+          store.commit("setId", response.data._id);
+          store.commit("setName", response.data.name);
+          store.commit("setToken", response.data.access_token);
+          store.commit("setRole", response.data.role);
+          SocketModule.connect();
           router.push({ name: "users" });
         }
       );
